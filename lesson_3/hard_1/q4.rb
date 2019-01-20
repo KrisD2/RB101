b000152b24035b126fd5d649b7f28a5585b6d704
Kris Denert <Kris.Denert@gmail.com>
2019-01-19 22:51:50 -0700

    Lesson 3 upload

---
+++ lesson_3/hard_1/q4.rb
@@ -0,0 +1,17 @@
+def dot_separated_ip_address?(input_string)
+  dot_separated_words = input_string.split(".")
+  return false unless dot_separated_words.size == 4
+  while dot_separated_words.size > 0 do
+    word = dot_separated_words.pop
+    return false unless is_an_ip_number?(word)
+  end
+  return true
+end
+
+def is_an_ip_number?(num)
+  (0..255).cover? num.to_i
+end
+
+p dot_separated_ip_address?("10.4.5.11")
+p dot_separated_ip_address?("4.5.5")
+p dot_separated_ip_address?("1.2.3.4.5")
